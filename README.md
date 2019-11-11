f2ec6042980d747df49f82b6fd573a84b4cd219e
Laurent G <xenlaurent@gmail.com>
2019-11-11 22:15:43 +0100

    Initial commit of my migration script in an early stage

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# mediawiki-2-hugo
+# MediaWiki-2-Hugo #
 Migration script from MediaWiki to Hugo
+
+## Intro ##
+This is a shell script I wrote to migrate my MediaWiki instance into a Hugo static website.
+
+In my use case, my MediaWiki instance was hosting several notes and HowTos. So, it was a quite small instance (256 pages in 8 NameSpaces and 16 Categories illustratated by 61 iamges). And even, it takes ~25sec to process. This is certainly not the best approach to migrate huge instance of MediaWiki. Nevertheless, as I din't found anything similar doing that, I think this script could really help other, even it should be improved.
+
+## How To use it? ##
+The useful variables are customizable via arguments of the shell script.
+```
+xenlo@red-carpet:~/Scripts/$ ./mediawiki-2-hugo.sh -h
+./mediawiki-2-hugo.sh : option non permise -- h
+Usage: ./mediawiki-2-hugo.sh [-v] [-i mediawiki_dir] [-o out_dir] [-t timezone] [-c charset]
+    -i mediawiki_dir   Specify the MediaWiki root directory as input (default: /var/www/mediawiki/)
+    -o out_dir         Specify output directory (default: ./out)
+    -t timezone        Specify your timezone offeset (default: +02:00)
+    -c charset         Specify the DB charset (default: binary)
+    -v                 Verbose
+```
+
+So simply run the script, eventually with sudo as apache user (if yours don't have read access to the medawiki directory).
+```
+sudo -u www-data ./mediawiki-2-hugo.sh -i /var/www/my_wiki -o /tmp/output
+```
+
+## What does this script? ##
+The script will first load the credentials from `LocalSettings.php` file and use it to read all wiki pages data in the database.
+From there, ti generates in a destination directory (`DEST_DIR` is set as `./out` by default) a structure of subdirectories which matches the [mediawiki namespaces](https://www.mediawiki.org/wiki/Manual:Namespace#Built-in_namespaces).
+
+Then for each wiki page (each record of `page` in the SQL DB) it will generate a `.md` file. And each file will be filled in with servral attributes ([front matters](https://gohugo.io/content-management/front-matter/) in Hugo language) and with the page's content.
+
+### Front Matters ###
+This script is fetching and filleing the following data:
+- **title**: Which is the `page.page_title` where I applay a replace of the '`_`' by spaces
+- **author**: The `user.user_name` of mediawiki user who created the page.
+- **date**: The oldest `revision.rev_timestamp` in the database for that page.
+- **lastmod**: The latest `revision.rev_timestamp` in the database for that page.
+- **draft**: Always false, ... (I think to put true for the stuff that are not from man name_space)
+- **categories**: The list of categories linked to that page (`categorylinks.cl_to`)
+- **tags**: Left empty for now...
+- **aliases**: The list of redirect/renammed page name (`page.page_title` when `page.page_is_redirect = 1`).
+
+### Content ###
+And of course, it fetch the content of each pages in its latest version, and convert it in MarkDown (github flavor).
+
+:warning: This use pandoc tool to convert the mediawiki syntax into the MarkDown. Please ensure that `pandoc` is accessible 
+
+## How does it works? / What does it need? ##
+
+### Requirements ###
+- [Pandoc](https://pandoc.org/) installed
+
+### Access ###
+- Read access to file `${WIKI_WEB_DIR}/LocalSettings.php`
+- Read access to directory `${WIKI_WEB_DIR}/images/`
+- SQL connectivity (credentials collected from `LocalSettings.php`)
+
+## What is the status of this ##
+It's working but far from perfect. Lot's of stuff are not handled as you can see in the list here under.
+
+### ToDo ###
+- Ensure it generate a nicer post header
+- Handle the verbosity
+- Manage template into html snippet
+- Further test of output in Hugo
+- Correct the following known bugs
+
+### Known Bugs ###
+- Don't handle the redirect of redirect ... (it throws a sed error)
+- Redirect don't work with namespace translations ... (it throws a sed error)
+- Remove the Category links from text
+- Several Code lines in place of a Code block
+- Espace insécable dans les Code block?
+
+### Later, Further Improvements ###
+- Complete the tags with ...?
+- Refactoring a bit with functions
+- Better exploit possibilities of pandoc ([metadata variables](https://pandoc.org/MANUAL.html#metadata-variables))??
+
+
+### Won't do ###
+- **Copy the files**: Except the images, I don't have any files on my wiki.
+- **Slug**: I took the page's name from mediawiki (which is the title with spaces subtitued by underscores) as files name. So the end of the url remain the same.
+- **Front matters of theme**: In the theme I plan to use ([Tranquilpeak](https://github.com/kakawait/hugo-tranquilpeak-theme/)) I don't saw any interesting attribute that could be feeded with data from the mediawiki.
+
+
+## License ##
+Copyright 2019 Laurent G (xenlo)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ mediawiki-2-hugo.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+
+#######
+#
+# Description:
+#   The goal of the script is to extract the data of a Mediawiki instance
+#   and convert it into .md files that you can inject in your Hugo
+#   static website.
+#
+# Credits:
+#    Author: Laurent G (xenlo)
+#    Source: TBD
+#    License: Apache License 2.0
+#
+
+usage(){
+    echo "Usage: ${0} [-v] [-i mediawiki_dir] [-o out_dir] [-t timezone] [-c charset]"
+    echo "    -i mediawiki_dir   Specify the MediaWiki root directory as input (default: /var/www/mediawiki/)"
+    echo "    -o out_dir         Specify output directory (default: ./out)"
+    echo "    -t timezone        Specify your timezone offeset (default: +02:00)"
+    echo "    -c charset         Specify the DB charset (default: binary)"
+    echo "    -v                 Verbose"
+    exit 1
+}
+
+validate_mediawiki_dir(){
+    if [[ ! -d ${1} ]]; then
+        echo "Error: ${1} is not a directory"
+        exit 1
+    fi
+    if [[ ! -f ${1}/LocalSettings.php ]]; then
+        echo "Error: File 'LocalSettings.php' not found in ${1}"
+        exit 1
+    fi
+}
+
+validate_out_dir(){
+    if [[ -d ${1} ]]; then
+        echo "Warning: ${1} already exist, this script may overwrite content!"
+        read -p "Do you want to proceed anyway? " -n 1 -r
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            echo "Cancelled"
+            exit 0
+        fi
+    fi
+}
+
+while getopts 'i:o:t:c:v' OPTION; do
+  case "$OPTION" in
+    i)
+        mediawiki_dir="${OPTARG}"
+        ;;
+    o)
+        out_dir="${OPTARG}"
+        ;;
+    t)
+        timezone="${OPTARG}"
+        ;;
+    c)
+        charset="${OPTARG}"
+        ;;
+    v)
+        verbose=true
+        ;;
+    ?)
+        usage
+        ;;
+  esac
+done
+
+DEBUG=${verobse:=false}
+
+WIKI_WEB_DIR=${mediawiki_dir:="/var/www/mediawiki/"}
+validate_mediawiki_dir ${WIKI_WEB_DIR}
+WIKI_WEB_DIR_ROOT=$(echo "${WIKI_WEB_DIR}" | cut -c 2-)
+
+DB_SERV=$(grep "wgDBserver" ${WIKI_WEB_DIR}/LocalSettings.php | cut -d\" -f2)
+DB_NAME=$(grep "wgDBname" ${WIKI_WEB_DIR}/LocalSettings.php | cut -d\" -f2)
+DB_LOGIN=$(grep "wgDBuser" ${WIKI_WEB_DIR}/LocalSettings.php | cut -d\" -f2)
+DB_PASS=$(grep '^\$wgDBpassword' ${WIKI_WEB_DIR}/LocalSettings.php | cut -d\" -f2)
+DB_CHARSET=${charset:="binary"}
+
+TIMEZONE_OFFSET=${timezone:="+02:00"}
+
+DEST_DIR=${out_dir:="./out"}
+validate_out_dir ${DEST_DIR}
+
+NAME_SPACES=(
+    [0]='main'
+    [1]='talk'
+    [2]='user'
+    [3]='user_talk'
+    [4]='project'
+    [5]='project_talk'
+    [6]='file'
+    [7]='file_talk'
+    [8]='mediawiki'
+    [9]='mediawiki_talk'
+    [10]='template'
+    [11]='template_talk'
+    [12]='help'
+    [13]='help_talk'
+    [14]='category'
+    [15]='category_talk'
+    )
+
+
+mapfile name_spaces_ids < <(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+    SELECT DISTINCT
+        page_namespace
+    FROM
+        page
+    ;")
+
+mapfile categories < <(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+    SELECT
+        *
+    FROM
+        category
+    ;")
+
+mapfile pages < <(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+    SELECT
+        p.page_id AS "page_id",
+        CAST(p.page_title AS ${DB_CHARSET}) AS "page_title",
+        p.page_namespace AS "page_namespace_id",
+        r.rev_text_id AS "revision_id",
+        DATE_FORMAT(r.rev_timestamp,'%Y-%m-%dT%H:%i:%s') AS "revision_timestamp",
+        t.old_id AS "text_id",
+        t.old_text AS "text_content",
+        u.user_name AS "user_name"
+    FROM
+        page p
+            INNER JOIN revision r
+                ON p.page_latest = r.rev_id
+            INNER JOIN text t
+                ON r.rev_text_id = t.old_id
+            INNER JOIN user u
+                ON r.rev_user = u.user_id
+    WHERE
+        p.page_is_redirect = 0
+    ;")
+
+mapfile redirects < <(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+    SELECT
+        CAST(p.page_title AS ${DB_CHARSET}) AS "redirect_title",
+        t.old_text AS "redirect_content"
+    FROM
+        page p
+            INNER JOIN revision r
+                ON p.page_latest = r.rev_id
+            INNER JOIN text t
+                ON r.rev_text_id = t.old_id
+    WHERE
+        p.page_is_redirect = 1
+    ;")
+
+echo "Ok, so we have ${#pages[@]} pages in ${#name_spaces_ids[@]} NameSpaces and ${#categories[@]} Categories to migrate..."
+echo ""
+
+echo "Creation of the output directory structure..."
+mkdir ${DEST_DIR}
+for name_space_id in "${name_spaces_ids[@]}"; do
+    mkdir ${DEST_DIR}/${NAME_SPACES[$name_space_id]}
+done
+echo ""
+
+echo "Creation of the output files..."
+for page in "${pages[@]}"; do
+    IFS=$'\t' read -r page_id page_title page_namespace_id revision_id revision_timestamp text_id text_content user_name <<< "${page}"
+
+    # Execute 2 extra SQL queries for the current page
+    creation_timestamp=$(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+        SELECT
+            DATE_FORMAT(MIN(rev_timestamp),'%Y-%m-%dT%H:%i:%s')
+        FROM
+            revision
+        WHERE
+            rev_page = ${page_id}
+        ;")
+    page_categories=$(mysql --host=${DB_SERV} --user=${DB_LOGIN} --password=${DB_PASS} --database=${DB_NAME} --batch --skip-column-names --execute="\
+        SELECT
+            CAST(cl_to AS ${DB_CHARSET})
+        FROM
+            categorylinks
+        WHERE
+            cl_from = ${page_id}
+        ;")
+    out_file=${DEST_DIR}/${NAME_SPACES[$page_namespace_id]}/${page_title//\//-}.md
+
+    # Generate the Front Matters
+    echo "  Generate ${out_file}"
+    echo "---"                                                >  ${out_file}
+    echo "title: \"${page_title//_/ }\""                      >> ${out_file}
+    echo "author: ${user_name}"                               >> ${out_file}
+    echo "date: ${creation_timestamp}${TIMEZONE_OFFSET}"      >> ${out_file}
+    echo "lastmod: ${revision_timestamp}${TIMEZONE_OFFSET}"   >> ${out_file}
+    echo "draft: false"                                       >> ${out_file}
+    echo "slug: " >> ${out_file}
+    echo "categories: "                                       >> ${out_file}
+    while read page_category; do
+        echo "  - ${page_category}"                           >> ${out_file}
+    done < <(printf '%s\n' "${page_categories}")
+    echo "tags: " >> ${out_file}
+    echo "aliases: " >> ${out_file}
+    echo "---" >> ${out_file}
+
+    if [[ ${NAME_SPACES[$page_namespace_id]} == "file" ]]; then
+        # Display the image in the corresponding .md file
+        echo "![${page_title}](images/${page_title//\//-})" >> ${out_file}
+    else
+        # Insert the context formated in MD
+        echo -e "${text_content}" | pandoc --from=mediawiki --to=markdown_github --atx-headers >> ${out_file}
+    fi
+    echo ""                                                   >> ${out_file}
+done
+
+echo "Add alias for each redirect"
+for redirect in "${redirects[@]}"; do
+    IFS=$'\t' read -r redirect_title redirect_content <<< "${redirect}"
+    cleanned_content=$(echo ${redirect_content} | sed -e 's/^#REDIRECT.*\[\[//' | sed -e 's/\]\].*$//' | sed -e 's/ /_/g')
+    if [[ ${cleanned_content} =~ ":" ]]; then
+        redirect_page=$(echo ${cleanned_content} | cut -d':' -f2)
+        redirect_namespace=$(echo ${cleanned_content} | cut -d':' -f1)
+        redirect_namespace=$(echo ${redirect_namespace} | sed 's/Discussion/talk/')
+    else
+        redirect_page=${cleanned_content}
+        redirect_namespace="main"
+    fi
+    out_file=${DEST_DIR}/${redirect_namespace}/${redirect_page//\//-}.md
+    echo "  Alias for ${out_file}"
+    sed -i -e "s/^aliases: /aliases: \n  - ${redirect_title}/" ${out_file}
+done
+echo ""
+
+echo "Copy the images"
+mkdir -p ${DEST_DIR}/images/
+for ext in png PNG jpg JPG jpeg JPEG gif GIF; do
+    find ${WIKI_WEB_DIR}/images/ -name "*.${ext}" \
+        -not -name "*[0-9]px-*" \
+        -not -path "*/deleted/*" \
+        -not -path "*/temp/*" \
+        -not -path "*/thumb/*" \
+        -not -path "*/archive/*" \
+        -exec cp {} ${DEST_DIR}/images/ \;
+done
+echo ""
+
