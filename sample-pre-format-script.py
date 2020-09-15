c8babf124d5bada0f3badf9052e15b701d94b5af
Laurent G <xenlaurent@gmail.com>
2020-09-15 21:53:08 +0200

    Improve the sample format script to fix issue #3

--- sample-pre-format-script.py
+++ sample-pre-format-script.py
@@ -9,9 +9,6 @@ import fileinput
 import re
 import sys
 
-preform_txt_regex = re.compile(r"^  ")
-code_block = ""
-
 """
 Walk through the input and replace the 'preformatted text' (starting with 2 spaces) 
 into 'Fixed width text' (<code>…</code>).
@@ -22,14 +19,38 @@ to:
   | <code>
   | echo "key: value" >> /etc/myDaemon.cfg
   | systemctl reload myDaemon
-  | </code>
+  | </code> 
 """
-for line in fileinput.input():
-  if line.startswith("  "):
-    code_block = code_block + preform_txt_regex.sub('', line)
-  else:
-    if code_block != "":
-        sys.stdout.write("<syntaxhighlight lang='shell'>\n{}</syntaxhighlight>\n".format(code_block))
-        code_block = ""
-    sys.stdout.write(line)
+def render_code_block(lines):
+  preform_txt_regex = re.compile(r"^  ")
+  code_block = []
+  output = []
+  for line in lines:
+    if line.startswith("  "):
+      code_block.append(preform_txt_regex.sub('', line))
+    else:
+      if code_block != []:
+        output.append("<syntaxhighlight lang='shell'>\n{}</syntaxhighlight>\n".format(code_block))
+        code_block = []
+      output.append(line)
+  return output
+
+"""
+Remove the all 'Category' tags
+"""
+def remove_category_tags(lines):
+  output = []
+  for line in lines:
+    line_no_cat = re.sub(r'\[\[Cat[ée]gor.*:[^\]]*]]', r'', line)
+    if line == "\n" or line_no_cat != "\n":
+      output.append(line_no_cat)
+  return output
+
+""" Main """
+lines_buffer = []
+lines_buffer = fileinput.input()
+lines_buffer = render_code_block(lines_buffer)
+lines_buffer = remove_category_tags(lines_buffer)
+for line in lines_buffer:
+  sys.stdout.write(line)
 
